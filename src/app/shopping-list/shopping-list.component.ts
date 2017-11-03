0a6f5d1a175d64ac3dc07e528e66408e5bd6c9a4
DESKTOP-GJMMA2C\Entronica_nut <nattaporn.k@entronica.co.th>
2017-11-03 13:01:03 +0700

    80. Allowing the User to Add Ingredients to the Shopping List

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,5 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
-
-import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -18,4 +17,7 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onIngredientAdded(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
+  }
 }
